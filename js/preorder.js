c504a76e2dd26ad2634bd4734cd21b1edffa7d60
Vitaly Pavlenko <cxielamiko@gmail.com>
2015-05-31 18:37:54 +0300

    Removed all old sections

--- js/preorder.js
+++ js/preorder.js
@@ -1,5 +1,5 @@
 $(function() {
-    var YANDEX_PREORDER_IFRAME_TEMPLATE = '<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/shop.xml?account=41001452948920&quickpay=shop&payment-type-choice=on&writer=seller&targets=Предоплата за {title}&targets-hint=&default-sum={price}&button-text=01&comment=on&hint=&fio=on&mail=on&phone=on&successURL=" width="450" height="253"></iframe>';
+    var YANDEX_PREORDER_IFRAME_TEMPLATE = '<iframe frameborder="0" allowtransparency="true" scrolling="no" src="https://money.yandex.ru/embed/shop.xml?account=41001452948920&quickpay=shop&payment-type-choice=on&writer=seller&targets=Предзаказ, {title}&targets-hint=&default-sum={price}&button-text=01&comment=on&hint=&fio=on&mail=on&phone=on&successURL=" width="450" height="253"></iframe>';
 
     function buildPreorderIframeForButton(button) {
         var price = button.attr('data-price');
